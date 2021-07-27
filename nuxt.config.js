5e82d597079af7c6e3716c2b898c0d153878b2ae
Jeremy Meiss <jeremy.meiss@gmail.com>
2021-07-27 14:58:11 -0500

    change name of page

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'orbit-signal',
+    title: 'circleci-orbit-signal',
     htmlAttrs: {
       lang: 'en',
     },
